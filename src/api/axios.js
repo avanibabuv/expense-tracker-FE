97b41773a5275263df6cbfa9257955cdc018f423
Avani V <avanibabuv@gmail.com>
2026-05-08 21:39:52 +0530

    first commit

---
+++ src/api/axios.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+import axios from "axios";
+
+const API = axios.create({
+    baseURL: "https://hashirmon22.pythonanywhere.com/"
+})
+
+// token attach
+API.interceptors.request.use((req)=>{
+    const token = localStorage.getItem("access")
+
+    if (token) {
+        req.headers.Authorization = `Bearer ${token}`
+    }
+
+    return req
+})
+
+// refresh token
+API.interceptors.response.use(
+    (response)=>response,
+    async (error) => {
+        const orginalRequest = error.config
+
+        if (error.response?.status == 401){
+            const refresh = localStorage.getItem("refresh");
+            const res = await axios.post(
+                "https://hashirmon22.pythonanywhere.com/api/token/refresh/", {refresh:refresh}
+            )
+            const newToken = res.data.access;
+            localStorage.setItem("access", newToken)
+            API.defaults.headers.Authorization = `Bearer ${newToken}`
+            orginalRequest.headers.Authorization = `Bearer ${newToken}`
+            return API(orginalRequest)
+        }
+
+        return Promise.reject(error)
+    }
+)
+
+
+export default API;
