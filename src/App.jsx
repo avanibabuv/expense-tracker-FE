4af45af72db0f5ffde3ab30d54e00dccab4c3a68
Avani V <avanibabuv@gmail.com>
2026-05-08 22:28:47 +0530

    loout fn

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import Login from "./components/LoginUi"
 function App() {
   const [user, setUser] = useState(() => !!localStorage.getItem("access"))
   return user ?
-    <ExpenseTracker/> :<Login onLogin={() => setUser(true)} />
-    
+    <ExpenseTracker onLogout={() => setUser(false)} /> : <Login onLogin={() => setUser(true)} />
+
 }
 
 export default App
